c8893634378f8dfa1e83044061b580310c7e3c32
yuen92 <31352233+imobo@users.noreply.github.com>
2021-01-20 18:31:50 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,37 @@
+# parameters for booking
+idcode = b"123"
+pw = b"456"
+bookname = "name"
+dept = "dept1"
+tel = 111
+
+# url for initialize cookies
+firstLoginUrl = r""
+# url for post a longin request
+loginUrl = r""
+# url for check login status
+LoginCheckUrl = r""
+# url for refresh Captcha
+imgUrl = r""
+# url for save Captcha image temporarily
+saveUrl = r"D:"
+# url for check the element in flame of booking page
+flameUrl = r""
+# url for check the booking details
+bookingUrl = r""
+# url for post data
+submitUrl = r""
+# url for query
+queryUrl = r""
+# url for cancel
+cancelUrl = r""
+
+
+# headers
+headers = { "Host": "11.111.111.111",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            }
