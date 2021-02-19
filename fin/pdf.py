3d13a67a59d2d134aefdcea4d19e90e8884f812d
yuen92 <31352233+imobo@users.noreply.github.com>
2021-02-19 10:47:52 +0800

    Update pdf.py

--- fin/pdf.py
+++ fin/pdf.py
@@ -1,1 +1,14 @@
+import math
+
+def normpdf(x, mu, sigma):
+    result = 1 / (sigma * math.sqrt(2 * math.pi)) * math.exp( - (x - mu) **2 / (2 * sigma **2))
+    return result
+
+"""
+def normpdf(x, mean, sd):
+    var = float(sd)**2
+    denom = (2*math.pi*var)**.5
+    num = math.exp(-(float(x)-float(mean))**2/(2*var))
+    return num/denom
+"""
 
